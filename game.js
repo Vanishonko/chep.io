51bd84cb2cc8fe8684e15d85a43f36aeaf50df56
vanishonko <vancocorcicous@gmail.com>
2021-08-16 19:52:34 +0300

    almost working rotation

--- game.js
+++ game.js
@@ -6,22 +6,22 @@ class Point {
 }
 
 
-// 1. Suzdavame promenlivi
 let player = {
     rotation: 90,
     duljina: 120,
     color: getRandomColor(), // rotation of the player in the range between 45 and 135 degrees. defaults to 90 when facing right;
     p1: new Point(35, 285),
     p2: new Point(35, 320),
-    p3: new Point(35, 285),
-    p4: new Point(35, 320),
+    p3: new Point(155, 285),
+    p4: new Point(155, 320),
+    p5: new Point(35, 303),
     draw: function(){
         context.fillStyle = this.color;
         context.beginPath();
         context.moveTo(this.p1.x, this.p1.y);
         context.lineTo(this.p2.x, this.p2.y);
-        context.lineTo(this.p4.x + this.duljina, this.p4.y);
-        context.lineTo(this.p3.x + this.duljina, this.p3.y);
+        context.lineTo(this.p4.x, this.p4.y);
+        context.lineTo(this.p3.x, this.p3.y);
         context.lineTo(this.p1.x, this.p1.y);
         context.closePath();
         context.fill();
@@ -32,19 +32,24 @@ let player = {
         context.fill();
     },
     aim: function(){
-        let dX = 35 - mouseX;
-        let dY = 303 - mouseY;
-        let length = Math.hypot(dX, dY);
-        if (length > 0){
-            dX /= length;
-            dY /= length;
+        let dX = mouseX - 35;
+        let dY = mouseY - 303;
+        this.rotation = Math.round( (Math.atan2(dY, dX) ) * 180 / Math.PI );
+        this.rotation += 90;
+        if(this.rotation < 0){
+            this.rotation += 360;
         }
-        this.rotation = Math.atan2(dX, dY) + 90*Math.PI/180;
+        if(this.rotation < 45){
+            this.rotation = 45;
+        }
+        if(this.rotation > 135){
+            this.rotation = 135;
+        }
+        console.log(this.rotation)
 
-        this.p3.x = rotate(35, 303, this.p3.x + this.duljina, this.p3.y, this.rotation)[0];
-        this.p3.y = -rotate(35, 303, this.p3.x + this.duljina, this.p3.y, this.rotation)[1];
-        this.p4.x = rotate(35, 303, this.p4.x + this.duljina, this.p4.y, this.rotation)[0];
-        this.p4.y = -rotate(35, 303, this.p4.x + this.duljina, this.p4.y, this.rotation)[1];
+        this.p3 = rotate(this.p5, this.p3, this.rotation);
+        this.p4 = rotate(this.p5, this.p4, this.rotation);
+    
     },
     shoot: function(){
 
@@ -52,7 +57,7 @@ let player = {
 }
 
 function update() {
-    // 2. Kodut tuk se izpulnqva 100
+   // console.log(dist(player.p1.x, player.p1.y, player.p3.x, player.p3.y));
 }
 
 function draw() {
@@ -60,15 +65,14 @@ function draw() {
 }
 
 function keyup(key) {
-    // Pechatai koda na natisnatiq klavish
     console.log("Pressed", key);
 }
 
 function mousemove(){
-    player.aim();
+     player.aim();
+
 }
 
 function mouseup() {
-    // Pri klik - pokaji koordinatite na mishkata
     console.log("Mouse clicked at", mouseX, mouseY);
 }
