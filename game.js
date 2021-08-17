f56568bd6b9a19d2990678aea2a18449e86a7957
Volen Nakov <volen.nakov@gmail.com>
2021-08-17 22:34:59 +0300

    Старт на нов branch

--- game.js
+++ game.js
@@ -1,47 +1,47 @@
 class Point {
-    constructor(x, y){
+    constructor(x, y) {
         this.x = x;
         this.y = y;
     }
 }
 
 class Eggcell {
-    constructor(){
-        this.x = 900 + Math.floor(Math.random()*300);
-        this.y = Math.floor(Math.random()*1000) - 400;
+    constructor() {
+        this.x = 900 + Math.floor(Math.random() * 300);
+        this.y = Math.floor(Math.random() * 1000) - 400;
         this.dX = 35 - this.x;
         this.dY = 303 - this.y;
         this.offScreen = false;
         let length = Math.hypot(this.dX, this.dY);
-        if(length > 0){
+        if (length > 0) {
             this.dX /= length;
             this.dY /= length;
         }
-        this.r = Math.floor(Math.random()*100) + 30;
+        this.r = Math.floor(Math.random() * 100) + 30;
         this.hp = this.r;
-        this.speed = 5 - this.r/100*5;
+        this.speed = 5 - this.r / 100 * 5;
         this.color = getRandomColor();
     }
-    move(){
+    move() {
         this.x += this.dX * this.speed;
         this.y += this.dY * this.speed;
-        if(this.x < - 200) this.offScreen = true;
+        if (this.x < - 200) this.offScreen = true;
     }
-    draw(){
+    draw() {
         context.fillStyle = this.color;
         context.beginPath();
-        context.arc(this.x, this.y, this.r, 0, Math.PI*2);
+        context.arc(this.x, this.y, this.r, 0, Math.PI * 2);
         context.closePath();
         context.fill();
         context.fillStyle = "gray";
-        context.fillRect(this.x - this.r, this.y - this.r - 14, this.r*2, 12)
+        context.fillRect(this.x - this.r, this.y - this.r - 14, this.r * 2, 12)
         context.fillStyle = this.color;
-        context.fillRect(this.x - this.r + 2, this.y - this.r - 12, this.hp*2 - 4, 8);
+        context.fillRect(this.x - this.r + 2, this.y - this.r - 12, this.hp * 2 - 4, 8);
     }
 }
 
 class Bullet {
-    constructor(point, dir, r){
+    constructor(point, dir, r) {
         this.x = point.x;
         this.y = point.y;
         this.r = r;
@@ -51,17 +51,17 @@ class Bullet {
         this.offScreen = false;
         this.dmg = 40;
         let length = Math.hypot(this.dX, this.dY);
-        if(length > 0) {
+        if (length > 0) {
             this.dX /= length;
             this.dY /= length;
         }
-        if(this.dX < 0) this.dX = 0;
-    }   
+        if (this.dX < 0) this.dX = 0;
+    }
 
     move() {
         this.x += this.dX * this.speed;
         this.y += this.dY * this.speed;
-        if(this.x > 1000 || this.y < -200 || this.y > 800) {
+        if (this.x > 1000 || this.y < -200 || this.y > 800) {
             this.offScreen = true;
         }
     }
@@ -70,7 +70,7 @@ class Bullet {
         context.fillStyle = "white";
         context.strokeStyle = "black";
         context.beginPath();
-        context.arc(this.x, this.y, this.r, 0, Math.PI*2);
+        context.arc(this.x, this.y, this.r, 0, Math.PI * 2);
         context.closePath();
         context.fill();
         context.stroke();
@@ -82,7 +82,7 @@ let player = {
     duljina: 120,
     color: getRandomColor(), // rotation of the player in the range between 45 and 135 degrees. defaults to 90 when facing right;
     p1: new Point(35, 303),
-    draw: function(){
+    draw: function () {
         context.fillStyle = this.color;
         context.save();
         context.translate(this.p1.x, this.p1.y);
@@ -90,27 +90,27 @@ let player = {
         context.fillRect(0, -18, this.duljina, 35);
         context.restore();
         context.beginPath();
-        context.arc(35, 285, 20, 0, Math.PI*2);
-        context.arc(35, 320, 20, 0, Math.PI*2);
+        context.arc(35, 285, 20, 0, Math.PI * 2);
+        context.arc(35, 320, 20, 0, Math.PI * 2);
         context.closePath();
         context.fill();
     },
-    aim: function(){
+    aim: function () {
         let dX = mouseX - 35;
         let dY = mouseY - 303;
-        this.rotation = Math.round( (Math.atan2(dY, dX) ) * 180 / Math.PI );
+        this.rotation = Math.round((Math.atan2(dY, dX)) * 180 / Math.PI);
         this.rotation += 90;
-        if(this.rotation < 0){
+        if (this.rotation < 0) {
             this.rotation += 360;
         }
-        if(this.rotation < 45 || ( mouseY < 303 && this.rotation > 135) ){
+        if (this.rotation < 45 || (mouseY < 303 && this.rotation > 135)) {
             this.rotation = 45;
         }
-        if(this.rotation > 135){
+        if (this.rotation > 135) {
             this.rotation = 135;
         }
     },
-    ejaculate: function(){
+    ejaculate: function () {
         let shootPoint;
         let oldP = new Point((35 + this.duljina), 303);
         shootPoint = rotate(this.p1, oldP, degToRad(this.rotation - 90));
@@ -121,19 +121,19 @@ let player = {
 
 let bullets = [], enemies = [], level = 0.975;
 function update() {
-    for ( let i = 0; i < bullets.length ; i ++){
+    for (let i = 0; i < bullets.length; i++) {
         bullets[i].move();
-        if(bullets[i].offScreen) bullets.splice(i, 1);
+        if (bullets[i].offScreen) bullets.splice(i, 1);
     }
     if (Math.random() > level) enemies.push(new Eggcell());
-    for ( let i = 0; i < enemies.length ; i ++) {
+    for (let i = 0; i < enemies.length; i++) {
         enemies[i].move();
-        if(enemies[i].offScreen) enemies.splice(i, 1);
-        for( let j = 0; j < bullets.length ; j ++){
-            if(dist(bullets[j].x, bullets[j].y, enemies[i].x, enemies[i].y) <= enemies[i].r + bullets[j].r){
+        if (enemies[i].offScreen) enemies.splice(i, 1);
+        for (let j = 0; j < bullets.length; j++) {
+            if (dist(bullets[j].x, bullets[j].y, enemies[i].x, enemies[i].y) <= enemies[i].r + bullets[j].r) {
                 enemies[i].hp -= bullets[j].dmg;
                 bullets.splice(j, 1);
-                if(enemies[i].hp <= 0) enemies.splice(i, 1);
+                if (enemies[i].hp <= 0) enemies.splice(i, 1);
             }
         }
     }
@@ -141,21 +141,21 @@ function update() {
 }
 
 function draw() {
-    for ( let i = 0; i < enemies.length ; i ++) enemies[i].draw();
-    for ( let i = 0; i < bullets.length ; i ++) bullets[i].draw();
+    for (let i = 0; i < enemies.length; i++) enemies[i].draw();
+    for (let i = 0; i < bullets.length; i++) bullets[i].draw();
     player.draw();
 }
 
 function keyup(key) {
-   // console.log("Pressed", key);
+    // console.log("Pressed", key);
     player.ejaculate();
 }
 
-function mousemove(){
+function mousemove() {
     player.aim();
 
 }
 
 function mouseup() {
-   // console.log("Mouse clicked at", mouseX, mouseY);
+    // console.log("Mouse clicked at", mouseX, mouseY);
 }
